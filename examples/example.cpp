67f3b29c8ce00f0543912bc6ad0eda15d5103ed4
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-02-19 21:01:07 +1300

    Expand and test example, remove old examples, add CI

--- examples/example.cpp
+++ examples/example.cpp
@@ -2,19 +2,18 @@
 // Created by Matthew Edwards on 19/02/19.
 //
 
-#include "wise_enum.h"
-
 #include <Arduino.h>
-
-
 #include <cassert>
 
-// Equivalent to enum Color {GREEN = 2, RED};
+#include "wise_enum.h"
+
+
+// Equivalent to enum Color {GREEN=2, RED};
 namespace my_lib {
 WISE_ENUM(Color, (GREEN, 2), RED)
 }
 
-// Equivalent to enum class MoreColor : int64_t {BLUE, BLACK = 1};
+// Equivalent to enum class MoreColor : int64_t {BLUE, BLACK=1};
 WISE_ENUM_CLASS((MoreColor, int64_t), BLUE, (BLACK, 1))
 
 // Inside a class, must use a different macro, but still works
@@ -24,16 +23,20 @@ struct Bar {
 
 // Adapt an existing enum you don't control so it works with generic code
 namespace another_lib {
-enum class SomebodyElse { FIRST, SECOND };
+enum class SomebodyElse {
+    FIRST, SECOND
+};
 }
 WISE_ENUM_ADAPT(another_lib::SomebodyElse, FIRST, SECOND)
 
+
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(9600);  // Teensy serial is always full USB speed, baud rate is ignored
+    while (!Serial) {};  // Wait for serial monitor
 
     // Number of enumerations:
     static_assert(wise_enum::size<my_lib::Color> == 2, "");
-    Serial.print("Number of enumerators: ");
+    Serial.print("Number of enumerations: ");
     Serial.println(wise_enum::size<my_lib::Color>);
 
     // Iterate over enums
@@ -43,10 +46,26 @@ void setup() {
         Serial.print(" ");
         Serial.println(e.name);
     }
+    for (auto e : wise_enum::range<MoreColor>) {
+        Serial.print(static_cast<int>(e.value));
+        Serial.print(" ");
+        Serial.println(e.name);
+    }
+    for (auto e : wise_enum::range<Bar::Foo>) {
+        Serial.print(static_cast<int>(e.value));
+        Serial.print(" ");
+        Serial.println(e.name);
+    }
+    for (auto e : wise_enum::range<another_lib::SomebodyElse>) {
+        Serial.print(static_cast<int>(e.value));
+        Serial.print(" ");
+        Serial.println(e.name);
+    }
     Serial.println();
 
     // Convert any enum to a string
     Serial.println(wise_enum::to_string(my_lib::Color::RED));
+    Serial.println();
 
     // Convert any string to an optional enum
     my_lib::Color x1, x2;
@@ -56,19 +75,23 @@ void setup() {
     assert(success1);
     assert(x1 == my_lib::Color::GREEN);
     assert(!success2);
+    Serial.print("GREEN is ");
+    Serial.println(static_cast<int>(x1));
+    Serial.println();
 
     // Everything is constexpr, and a type trait is made available for easy use in
     // enable_if/tag dispatch
     static_assert(wise_enum::is_wise_enum_v<my_lib::Color>, "");
     static_assert(!wise_enum::is_wise_enum_v<int>, "");
-    enum flub { blub, glub };
+    enum flub {
+        blub, glub
+    };
     static_assert(!wise_enum::is_wise_enum_v<flub>, "");
     // We made a regular enum wise!
     static_assert(wise_enum::is_wise_enum_v<another_lib::SomebodyElse>, "");
 
     // Assert underlying type
-    static_assert(std::is_same<int64_t, std::underlying_type_t<MoreColor>>::value,
-                  "");
+    static_assert(std::is_same<int64_t, std::underlying_type_t<MoreColor>>::value, "");
 
     // Full API available for adapted wise enums
     for (auto e : wise_enum::range<another_lib::SomebodyElse>) {
